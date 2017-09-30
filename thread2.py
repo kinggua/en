b7eb98df8b05c03fc5a7ad179a40e898daf20b69
kinggua <zhangxddawang@126.com>
2017-09-30 14:07:12 +0800

    线程数可控

--- thread2.py
+++ thread2.py
@@ -1,2 +1,21 @@
 # !/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+from concurrent.futures import ThreadPoolExecutor
+
+# 自定义线程池最大数量
+pool = ThreadPoolExecutor(max_workers=11)
+
+
+def your_function(imgurl, imgname):
+    pass
+
+
+# 将下载任务加入线程
+imgs = []
+for img in imgs:
+    imgurl = img.url
+    imgname = img.name
+    pool.submit(your_function, imgurl, imgname)
+
+
