30d89ac509a0b9616c3c48de034391a2c189a07e
kinggua <zhangxddawang@126.com>
2017-09-30 11:35:06 +0800

    just

--- thread.py
+++ thread.py
@@ -16,39 +16,59 @@ header = {'User-Agent': userAgent,
 html_byte = requests.get(url=url, headers=header).content
 html = html_byte.decode("UTF-8")
 
+
 def aruqreurl(html):
     if None == html:
         print("error in html build")
+        return None
     else:
+        soup = BeautifulSoup(html, "html.parser")
+        imgs = soup.select("body img")
+        for img in imgs:
+            if "http" in img['src']:
+                continue
+            else:
+                img['src'] = "http:" + img['src']
+        return imgs
+
+
+imgurls = aruqreurl(html)
 
 
 # 定义线程
 def download(imgurl, imgname):
+    print(imgurl)
     header = {'User-Agent': userAgent,
               'Referer': imgurl,
               'Host': imgurl.split("/")[2]}
-    saveimg = open("E:/pypic" + str(imgname), "wb")
+    saveimg = open("E:/pypic/" + str(imgname), "wb")
     saveimg.write(requests.get(url=imgurl, headers=header).content)
     saveimg.close()
 
+
 # 创建线程池
 threadpool = []
-imgurls = []
-imgname = 1
 for imgurl in imgurls:
-
-    th = threading.Thread(target=download, args=(imgurl, imgname))
+    img = imgurl["src"]
+    imgname = img.split("/")[-1]
+    print(img)
+    th = threading.Thread(target=download, args=(imgurl["src"], imgname))
     # 将线程加入线程池
     threadpool.append(th)
-    imgname += 1
+
 
 # 开始线程
+
 for th in threadpool:
     th.start()
 
 # 等待所有线程运行完毕
+count =1
 for th in threadpool:
+    count +=1
     th.join()
 
+
+print(count)
 
 
